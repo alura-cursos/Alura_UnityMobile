fe9b3d2f563fb4c3c1caf9e42fb70125c42998f6
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-07-23 10:45:35 -0300

    Projeto Inicial

---
+++ Assets/Scritpt/Gameplay/Gerador.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Gerador : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefabInimigo;
+    [SerializeField]
+    private float tempo;
+    [SerializeField]
+    private float raio;
+
+
+
+    private void Start()
+    {
+        
+        this.InvokeRepeating("Instanciar", 0, this.tempo);
+    }
+
+    private void Instanciar()
+    {
+        var inimigo = GameObject.Instantiate(this.prefabInimigo);
+        this.DefinirPosicaoInimigo(inimigo);
+    }
+
+    private void DefinirPosicaoInimigo(GameObject inimigo)
+    {
+        var posicaoAleatoria = new Vector3(
+                        Random.Range(-this.raio, this.raio),
+                        Random.Range(-this.raio, this.raio),
+                        0);
+
+        var posicaoInimigo = this.transform.position + posicaoAleatoria;
+        inimigo.transform.position = posicaoInimigo;
+    }
+}
---
+++ Assets/Scritpt/LerCena.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LerCena : MonoBehaviour {
+
+	public void CarregarCena(string cena)
+    {
+        SceneManager.LoadScene(cena);
+    }
+}
