bcbd878301ca0a13ac0b5b354af22be6030afc90
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-07-23 11:36:55 -0300

    Video 1.2

---
+++ Assets/Scritpt/Gameplay/SegueMouse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SegueMouse : MonoBehaviour {
+
+	private void Update () {
+        var posicao = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.transform.position = posicao;
+	}
+}
