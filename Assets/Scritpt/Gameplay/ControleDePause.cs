728a8ddda53ed9921e952437d19b9cd00b3be564
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-07-25 11:30:23 -0300

    Video 2.2

---
+++ Assets/Scritpt/Gameplay/ControleDePause.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleDePause : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject painelPause;
+
+    private void Update () {
+        if (this.EstaoTocandoNaTela())
+        {
+            this.ContinuarJogo();
+        }
+        else
+        {
+            this.PararJogo();
+        }
+	}
+
+    private void ContinuarJogo()
+    {
+        this.painelPause.SetActive(false);
+        
+    }
+
+    private void PararJogo()
+    {
+        this.painelPause.SetActive(true);
+        
+    }
+
+    private bool EstaoTocandoNaTela()
+    {
+        return Input.touchCount > 0;
+    }
+}
