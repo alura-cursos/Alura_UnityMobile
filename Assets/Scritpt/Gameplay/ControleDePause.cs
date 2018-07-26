766e032fc971c4ca13d140ab126b13acaf711b6e
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-07-26 09:43:46 -0300

    Video 2.3

--- Assets/Scritpt/Gameplay/ControleDePause.cs
+++ Assets/Scritpt/Gameplay/ControleDePause.cs
@@ -6,6 +6,8 @@ public class ControleDePause : MonoBehaviour {
 
     [SerializeField]
     private GameObject painelPause;
+    [SerializeField, Range(0,1)]
+    private float escalaDeTempoDuranteOPause;
 
     private void Update () {
         if (this.EstaoTocandoNaTela())
@@ -21,17 +23,23 @@ public class ControleDePause : MonoBehaviour {
     private void ContinuarJogo()
     {
         this.painelPause.SetActive(false);
-        
+        this.MudarEscalaDeTempo(1);
     }
 
     private void PararJogo()
     {
         this.painelPause.SetActive(true);
-        
+        this.MudarEscalaDeTempo(this.escalaDeTempoDuranteOPause);
     }
 
     private bool EstaoTocandoNaTela()
     {
         return Input.touchCount > 0;
+    }
+
+    private void MudarEscalaDeTempo(float escala)
+    {
+        Time.timeScale = escala;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
     }
 }
