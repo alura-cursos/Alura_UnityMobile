71cb39bcd60f1a11f7e5e59236b42442af3e3346
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-08-24 09:17:21 -0300

    Unica Instancia

---
+++ Assets/Scritpt/UnicaInstancia.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnicaInstancia : MonoBehaviour
+{
+    private void Start()
+    {
+        var outros = GameObject.FindGameObjectsWithTag(this.tag);
+
+        for (var i = 0; i < outros.Length; i++)
+        {
+            var outro = outros[i];
+
+            if (outro != this.gameObject)
+            {
+                GameObject.Destroy(outro);
+            }
+        }
+    }
+}
